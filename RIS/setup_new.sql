6c58f850244bfe7c3d232b7fcfaea299906bdf8e
jsurya <jsurya@ualberta.ca>
2014-03-12 20:04:52 -0600

    added granting to script

--- RIS/setup_new.sql
+++ RIS/setup_new.sql
@@ -4,24 +4,24 @@
  *              Winter Term, 2014
  *  Author:     Prof. Li-Yan Yuan
  */
-DROP TABLE IF EXISTS family_doctor;
-DROP TABLE IF EXISTS pacs_images;
-DROP TABLE IF EXISTS radiology_record;
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS persons;
+DROP TABLE family_doctor;
+DROP TABLE pacs_images;
+DROP TABLE radiology_record;
+DROP TABLE users;
+DROP TABLE persons;
 
 /*
  *  To store the personal information
  */
 CREATE TABLE persons (
-   `person_id` int(7),
-   `first_name` varchar(24),
-   `last_name`  varchar(24),
-   `address`   varchar(128),
-   `email`      varchar(128),
-   `phone`      char(10),
-   PRIMARY KEY(`person_id`),
-   UNIQUE (`email`)
+   person_id int,
+   first_name varchar(24),
+   last_name  varchar(24),
+   address    varchar(128),
+   email      varchar(128),
+   phone      char(10),
+   PRIMARY KEY(person_id),
+   UNIQUE (email)
 );
 
 /*
@@ -30,55 +30,74 @@ CREATE TABLE persons (
  *  on his/her role in the log-in  
  */
 CREATE TABLE users (
-   `user_name` varchar(24),
-   `password`  varchar(24),
-   `class`     char(1),
-   `person_id` int(7),
-   `date_registered` date,
+   user_name varchar(24),
+   password  varchar(24),
+   class     char(1),
+   person_id int,
+   date_registered date,
    CHECK (class in ('a','p','d','r')),
-   PRIMARY KEY(`user_name`),
-   FOREIGN KEY (`person_id`) REFERENCES `persons` (`person_id`)
+   PRIMARY KEY(user_name),
+   FOREIGN KEY (person_id) REFERENCES persons
 );
 
 /*
  *  to indicate who is whose family doctor.
  */
 CREATE TABLE family_doctor (
-   `doctor_id`    int(7),
-   `patient_id`   int(7),
-   FOREIGN KEY(`doctor_id`) REFERENCES `persons` (`person_id`),
-   FOREIGN KEY(`patient_id`) REFERENCES `persons`(`person_id`),
-   PRIMARY KEY(`doctor_id`,`patient_id`)
+   doctor_id    int,
+   patient_id   int,
+   FOREIGN KEY(doctor_id) REFERENCES persons,
+   FOREIGN KEY(patient_id) REFERENCES persons,
+   PRIMARY KEY(doctor_id,patient_id)
 );
 
 /*
  *  to store the radiology records
  */
 CREATE TABLE radiology_record (
-   `record_id`   int(5),
-   `patient_id`  int(7),
-   `doctor_id`   int(7),
-   `radiologist_id` int(7),
-   `test_type`   varchar(24),
-   `prescribing_date` date,
-   `test_date`    date,
-   `diagnosis`    varchar(128),
-   `description`   varchar(1024),
-   PRIMARY KEY(`record_id`),
-   FOREIGN KEY(`patient_id`) REFERENCES `persons` (`person_id`),
-   FOREIGN KEY(`doctor_id`) REFERENCES  `persons` (`person_id`),
-   FOREIGN KEY(`radiologist_id`) REFERENCES `persons` (`person_id`)
+   record_id   int,
+   patient_id  int,
+   doctor_id   int,
+   radiologist_id int,
+   test_type   varchar(24),
+   prescribing_date date,
+   test_date    date,
+   diagnosis    varchar(128),
+   description   varchar(1024),
+   PRIMARY KEY(record_id),
+   FOREIGN KEY(patient_id) REFERENCES persons,
+   FOREIGN KEY(doctor_id) REFERENCES  persons,
+   FOREIGN KEY(radiologist_id) REFERENCES  persons
 );
 
 /*
  *  to store the pacs images
  */
 CREATE TABLE pacs_images (
-   `record_id`   int(5),
-   `image_id`    int(5),
-   `thumbnail`   blob,
-   `regular_size` blob,
-   `full_size`    blob,
-   PRIMARY KEY(`record_id`,`image_id`),
-   FOREIGN KEY(`record_id`) REFERENCES `radiology_record` (`record_id`)
+   record_id   int,
+   image_id    int,
+   thumbnail   blob,
+   regular_size blob,
+   full_size    blob,
+   PRIMARY KEY(record_id,image_id),
+   FOREIGN KEY(record_id) REFERENCES radiology_record
 );
+
+/*
+ *  Grant privileges to group members
+ */
+GRANT ALL ON users TO qali;
+GRANT ALL ON family_doctor TO qali;
+GRANT ALL ON radiology_record TO qali;
+GRANT ALL ON pacs_images TO qali;
+
+
+GRANT ALL ON users TO jsurya;
+GRANT ALL ON family_doctor TO jsurya;
+GRANT ALL ON radiology_record TO jsurya;
+GRANT ALL ON pacs_images TO jsurya;
+
+/*
+ *  to access tables:
+ *    ALTER SESSION SET current_schema = <Member1>;
+ */
