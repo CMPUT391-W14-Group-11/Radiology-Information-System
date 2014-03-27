6a1a8d398b7b680439a91b04684b2adc8c761620
jsurya <jsurya@ualberta.ca>
2014-03-26 21:26:31 -0600

    Inserts new entries into persons table; errors with inserting users due to size of char

--- WEB-INF/classes/UserRegistrationServlet.java
+++ WEB-INF/classes/UserRegistrationServlet.java
@@ -1,33 +1,20 @@
-package main;
-
 import java.io.*;
+import java.sql.*;
+import java.util.*;
+import java.lang.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
-import java.util.*;
-import java.sql.*;
-
-import main.*;
 
 /**
  * Servlet implementation class UserRegistrationServlet
  */
 public class UserRegistrationServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public UserRegistrationServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	       	
+	       	HttpSession session = request.getSession();
 	       	response.setContentType("text/html");
 
 	}
@@ -36,7 +23,22 @@ public class UserRegistrationServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		createUserAccount(request);
+		HttpSession session = request.getSession();
+
+		String message = "OK " + createUserAccount(request);;
+		PrintWriter out = response.getWriter();
+      		out.println("<h1>" + message + "</h1>");
+      		out.println("<body><ul>");
+      		out.println("<li>" + request.getParameter("username") + "</li>");
+      		out.println("<li>" + request.getParameter("password2") + "</li>");
+      		out.println("<li>" + (request.getParameter("class")).charAt(0) + "</li>");
+      		out.println("<li>" + request.getParameter("first-name") + "</li>");
+      		out.println("<li>" + request.getParameter("last-name") + "</li>");
+      		String addr =  request.getParameter("address") + "";
+      		out.println("<li>" + addr  + "</li>");
+      		out.println("<li>" + request.getParameter("email") + "</li>");
+      		out.println("<li>" + request.getParameter("phone") + "</li>");
+      		out.println("</body>");
 
 	}
 
@@ -49,22 +51,40 @@ public class UserRegistrationServlet extends HttpServlet {
 	 * @return int success
 	 */
 	public int createUserAccount(HttpServletRequest request) {
-		User user = new User();
 
-		user.setUserID(request.getParameterValues("username"));
-		user.setPassword(request.getParameterValues("password2"));
-		user.setUserClass((request.getParameterValues("class")).charAt(0));
-
-		user.setFirstName(request.getParameterValues("first-name"));
-		user.setLastName(request.getParameterValues("last-name"));
-		user.setAddress(request.getParameterValues("address"));
-		user.setEmail(request.getParameterValues("email"));
-		user.setPhone(request.getParameterValues("phone"));
-
+		String user_name = request.getParameter("username");
+		String password = request.getParameter("password2");
+		Character user_class = (request.getParameter("class")).charAt(0);
+		
 		Db database = new Db();
-		user.setPersonID(database.getNextPeronID()); 
-		database.createUserAccount(user);
+		int person_id = database.getNextPersonID();
 
-		return 0;
+		User user = new User(user_name, password, user_class, person_id);
+
+		user.setFirstName(request.getParameter("first-name"));
+		user.setLastName(request.getParameter("last-name"));
+		
+		String addr =  request.getParameter("address");
+		if (addr == null) {
+			user.setAddress("");
+		}
+		else {
+			user.setAddress(addr);
+		}
+		
+		user.setEmail(request.getParameter("email"));
+		
+		String ph =  request.getParameter("phone");
+		if (addr == null) {
+			user.setPhone("");
+		}
+		else {
+			user.setPhone(ph);
+		}
+
+		int result = database.insertUserAccount(user);
+
+		database.close();
+		return result;
 	}
 }
