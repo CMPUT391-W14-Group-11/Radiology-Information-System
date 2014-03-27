dcc7ea1104c3525ad8963cbc6115b694aef71f98
jsurya <jsurya@ualberta.ca>
2014-03-26 21:21:10 -0600

    SQL script for making admin

---
+++ admin.sql
@@ -0,0 +1,7 @@
+INSERT INTO persons VALUES (
+	0, 'administrator', 'administrator', 'address', 'admin@ris.com', 'phone'
+);
+
+INSERT INTO users VALUES (
+	'admin', 'password', 'a', 0, CURRENT_DATE
+);
